96df027a091f80d7847dddf8b029fe3289789f07
pwhite <Patrick White>
2015-11-11 14:00:19 -0500

    adding some stuff

--- logstash/startup.sh
+++ logstash/startup.sh
@@ -2,11 +2,8 @@
 
 sudo docker build -t logstash-sb . \
 
-<<<<<<< HEAD
 sudo docker run -d --rm \
-=======
 sudo docker run -d \
->>>>>>> master
   -v "$PWD"/config:/config \
   --net=host \
   --name logstash-sb \
--- swarm_commands.sh
+++
@@ -1,6 +0,0 @@
-sudo docker-machine create -d virtualbox elk
-eval ''
-eval 'dollarparensudo docker-machine env elk)'
-sudo docker run swarm create
-docker-machine create -d virtualbox --swarm --swarm-master --swarm-discovery token://2f80a8de0fed15b7c361d706dd0860fd swarm-master
-docker-machine create -d virtualbox --swarm --swarm-discovery token://2f80a8de0fed15b7c361d706dd0860fd elastic-sb
